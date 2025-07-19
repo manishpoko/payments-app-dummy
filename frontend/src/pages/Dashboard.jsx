130bd757da8310068d1e38cd1e61101bcb61237f
Manish <manishpokhrel313@gmaail.com>
2025-07-19 23:57:13 +0530

    feat: Build frontend UI and components for auth and dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,28 @@
-export function Dashboard() {
-    return <div>Dashboard Page</div>
+import { useEffect, useState } from "react";
+import { Appbar } from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
+import axios from "axios";
+
+export const Dashboard = () => {
+    const [balance, setBalance] = useState(0);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/account/balance", {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        })
+        .then(response => {
+            setBalance(response.data.balance)
+        })
+    }, []); // The empty array makes this run only once when the component mounts
+
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={balance} />
+            <Users />
+        </div>
+    </div>
 }
